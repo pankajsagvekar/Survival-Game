97b0c01264da6037b169b7df824499ace368b47a
Pankaj Sagvekar <pankajsagvekar167@gmail.com>
2024-06-26 23:01:48 +0530

    animation

--- Enemy/enemy_generic.gd
+++ Enemy/enemy_generic.gd
@@ -2,9 +2,20 @@ extends CharacterBody2D
 
 @export var movement_speed = 20.0
 
-@onready var player = get_tree().get_first_node_in_group("player") #get group player coordinates, Group is defined in the players group tab
+
+@onready var player = get_tree().get_first_node_in_group("player") #get group player
+@onready var sprite = $Sprite2D
+@onready var animation = $AnimationPlayer #using animationplayer for enemy because the animation is continous
+
+func _ready(): #using ready beacuse it runs once at the beginning
+	animation.play("walk")
 
 func _physics_process(__delta): #__delta because we are not using it
 	var direction = global_position.direction_to(player.global_position) #get direction of player and follow him
 	velocity = direction * movement_speed
 	move_and_slide()
+	
+	if direction.x > 0.1:
+		sprite.flip_h = true
+	elif direction.x  < -0.1:
+		sprite.flip_h = false
--- Player/player.gd
+++ Player/player.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
 var movement_speed = 50.0
+@onready var sprite = $Sprite2D
+@onready var walkTimer = get_node("walkTimer")
 
 func _physics_process(delta):
 	movement()
@@ -9,5 +11,18 @@ func movement():
 	var x_mov = Input.get_action_strength("right") - Input.get_action_strength("left") #outputs 0 or 1 if false or true 
 	var y_mov = Input.get_action_strength("down") - Input.get_action_strength("up") #the all inputs are already defined in project setting in the input mapping tab
 	var mov = Vector2(x_mov, y_mov) #this give the direction of x and y axis for eg: (3, 5)
+	if mov.x > 0:
+		sprite.flip_h = true
+	elif mov.x < 0:
+		sprite.flip_h = false
+		
+	if mov != Vector2.ZERO:
+			if walkTimer.is_stopped():
+				if sprite.frame >= sprite.hframes - 1:
+					sprite.frame = 0
+				else:
+					sprite.frame += 1
+				walkTimer.start()
+	
 	velocity = mov.normalized()*movement_speed #the velocity is the inbuild function in the godot by which we are manipulating the direction of the character by inputing the vector in it and multiplying with the speed
 	move_and_slide() #used to move our character
